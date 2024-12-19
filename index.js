d6f1557927f29574968f322ae256e6d50c414288
brenoiscde <breninhoponticelli@gmail.com>
2024-12-18 22:48:33 -0300

    adicionado a logica de repeticao de projeto

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { listProjects } from "./mock/projects.js";
+
 function removeLi(){
     if(window.innerWidth <= 1000){
         const liDepoimento = document.querySelector('ul li:nth-child(6)');
@@ -20,3 +22,26 @@ function removeLi(){
 }
 removeLi();
 window.addEventListener('resize', removeLi);
+
+function renderProjects(projects, divProjects){
+    let divProjetcts = document.querySelector(divProjects);
+    divProjetcts.innerHTML += projects.map((project)=>{
+        return`
+            <div class="portfolio__project">
+                <div class="portfolio__project__header">
+                    <h2>${project.nome}</h2>
+                    <img src="/assets/github-142-svgrepo-com (1).svg" alt="icon-github">
+                </div>
+                <h3>Descrição do projeto</h3>
+                <p>${project.description}</p>
+                <div class="portfolio__project__tecnology">
+                    <span>${project.listTec.join(' ')}</span>
+                </div>
+                <a href="">Ver projeto</a>
+                <img class="portfolio__project__image" src=${project.photo} alt="image-project">
+            </div>
+        `
+    })
+}
+renderProjects(listProjects,".portfolio__projects")
+
---
+++ mock/projects.js
@@ -0,0 +1,9 @@
+ export const listProjects = [
+    {
+        nome: "Card Yellow",
+        description: "Esse projeto foi criado para praticar minha habilidades básicas de html e css.Onde foi Desenvolvido um card com elementos dentro e um background.",
+        listTec : ["HTML", "CSS"],
+        photo : "/assets/cardyellow.webp"
+    }
+]
+
